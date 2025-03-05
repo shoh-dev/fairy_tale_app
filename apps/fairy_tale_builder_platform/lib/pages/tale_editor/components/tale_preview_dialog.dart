0d26a562ff835b0f9de812f6839f44e5e5974287
Shohjahon <komiljonovshohjahon@gmail.com>
2025-03-05 12:51:00 +0500

    Add device preview

--- apps/fairy_tale_builder_platform/lib/pages/tale_editor/components/tale_preview_dialog.dart
+++ apps/fairy_tale_builder_platform/lib/pages/tale_editor/components/tale_preview_dialog.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:fairy_tale_builder_platform/components/translator_component.dart';
 import 'package:fairy_tale_builder_platform/manager/redux/state.dart';
 import 'package:fairy_tale_builder_platform/manager/selector.dart';
@@ -35,73 +36,76 @@ class TalePreviewDialog extends StatelessWidget {
               ),
             ],
           ),
-          content: Container(
+          content: SizedBox(
             width: tale.isPortrait ? 480 : 720,
             height: tale.isPortrait ? 720 : 480,
-            decoration: BoxDecoration(
-              border: Border.all(color: context.colorScheme.primary),
-            ),
             child: page != null
-                ? Stack(
-                    children: [
-                      //image
-                      if (page.backgroundImage.isNotEmpty)
-                        Positioned.fill(
-                          child: Image.network(
-                            page.backgroundImage,
-                            fit: BoxFit.cover,
+                ? DeviceFrame(
+                    device: Devices.ios.iPhone13,
+                    orientation: tale.isPortrait
+                        ? Orientation.portrait
+                        : Orientation.landscape,
+                    screen: Stack(
+                      children: [
+                        //image
+                        if (page.backgroundImage.isNotEmpty)
+                          Positioned.fill(
+                            child: Image.network(
+                              page.backgroundImage,
+                              fit: BoxFit.cover,
+                            ),
                           ),
-                        ),
 
-                      for (final interaction in page.taleInteractions)
-                        //tale object
-                        AnimatedPositioned(
-                          // curve: Curves.easeInOutCubicEmphasized,//todo: get curve from db
-                          duration: Duration(
-                            milliseconds: interaction.animationDuration,
-                          ),
-                          width: interaction.size.width,
-                          height: interaction.size.height,
-                          left: interaction.currentPosition.dx,
-                          top: interaction.currentPosition.dy,
-                          child: MouseRegion(
-                            cursor: SystemMouseCursors.click,
-                            child: Translator(
-                              toTranslate: [
-                                interaction.hintKey,
-                              ],
-                              builder: (translatedValue) => Tooltip(
-                                message: translatedValue[0],
-                                showDuration: const Duration(seconds: 5),
-                                child: Container(
-                                  width: interaction.size.width,
-                                  height: interaction.size.height,
-                                  decoration: !interaction.metadata.hasImage
-                                      ? BoxDecoration(
-                                          border: Border.all(),
-                                          borderRadius:
-                                              BorderRadius.circular(10),
-                                          boxShadow: [
-                                            BoxShadow(
-                                              color:
-                                                  Colors.black.withAlpha(100),
-                                              blurRadius: 10,
-                                              spreadRadius: 2,
-                                            ),
-                                          ],
-                                        )
-                                      : null,
-                                  child: interaction.metadata.hasImage
-                                      ? Image.network(
-                                          interaction.metadata.imageUrl,
-                                        )
-                                      : null,
+                        for (final interaction in page.taleInteractions)
+                          //tale object
+                          AnimatedPositioned(
+                            // curve: Curves.easeInOutCubicEmphasized,//todo: get curve from db
+                            duration: Duration(
+                              milliseconds: interaction.animationDuration,
+                            ),
+                            width: interaction.size.width,
+                            height: interaction.size.height,
+                            left: interaction.currentPosition.dx,
+                            top: interaction.currentPosition.dy,
+                            child: MouseRegion(
+                              cursor: SystemMouseCursors.click,
+                              child: Translator(
+                                toTranslate: [
+                                  interaction.hintKey,
+                                ],
+                                builder: (translatedValue) => Tooltip(
+                                  message: translatedValue[0],
+                                  showDuration: const Duration(seconds: 5),
+                                  child: Container(
+                                    width: interaction.size.width,
+                                    height: interaction.size.height,
+                                    decoration: !interaction.metadata.hasImage
+                                        ? BoxDecoration(
+                                            border: Border.all(),
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            boxShadow: [
+                                              BoxShadow(
+                                                color:
+                                                    Colors.black.withAlpha(100),
+                                                blurRadius: 10,
+                                                spreadRadius: 2,
+                                              ),
+                                            ],
+                                          )
+                                        : null,
+                                    child: interaction.metadata.hasImage
+                                        ? Image.network(
+                                            interaction.metadata.imageUrl,
+                                          )
+                                        : null,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                    ],
+                      ],
+                    ),
                   )
                 : Center(
                     child: Text(
